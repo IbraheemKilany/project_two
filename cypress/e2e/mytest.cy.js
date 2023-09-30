cda7e76b97e013bd997ca6ec80e2acdbc0627aa3
ibraheem <kilany970@gmail.com>
2023-10-01 02:51:46 +0300

    update on the language when website ar or en and chose second options from hotels

--- cypress/e2e/mytest.cy.js
+++ cypress/e2e/mytest.cy.js
@@ -1,5 +1,26 @@
+/// <reference types="cypress"/>
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    // cy.visit('https://www.almosafer.com/ar')
+    // cy.visit('https://www.almosafer.com/en')
+    const websites=["https://www.almosafer.com/ar","https://www.almosafer.com/en"]
+    const randomIndex= Math.floor(Math.random()*websites.length)
+    cy.visit(websites[randomIndex])
+    cy.get('.cta__saudi').click()
+    // cy.get('#uncontrolled-tab-example-tab-hotels > .sc-kOnlKp').click()
+    cy.get('#uncontrolled-tab-example-tab-hotels').click()
+    if(randomIndex==0){
+
+      cy.get('[data-testid="AutoCompleteInput"]').type("دبي")
+      cy.get('[data-testid="AutoCompleteResultItem1"] > .sc-12clos8-5').click()
+    }
+    else if(randomIndex==1){
+      cy.get('[data-testid="AutoCompleteInput"]').type("dubai")
+      cy.wait(1000)
+      cy.get('[data-testid="AutoCompleteResultItem1"] > .sc-12clos8-5').click()
+    }
+    
+
+    })
   })
-})
